73f0ed841b72ea87f9a6634539db7c300d44a4f1
Mark-Weston <markweston@cock.li>
2017-10-11 17:33:03 +0300

    Install/unintall

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cp bin/stopwatch /usr/bin/stopwatch
+cp etc/stopwatch.desktop /usr/share/applications/stopwatch.desktop
+cp etc/stopwatch.png /usr/share/pixmaps/stopwatch.png
---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+rm -f /usr/bin/stopwatch
+rm -f /usr/share/applications/stopwatch.desktop
+rm -f /usr/share/pixmaps/stopwatch.png
+rm -f /usr/local/bin/stopwatch
+rm -f /usr/local/share/applications/stopwatch.desktop
+rm -f /usr/local/share/pixmaps/stopwatch.png
