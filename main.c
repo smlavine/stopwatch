8fff7af4df01aa993a308d665ff4272e489a3f71
Mark-Weston <markweston@cock.li>
2017-10-20 22:12:03 +0300

    Clear screen

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
+#define GLFW_INCLUDE_ES2
 #include <GLFW/glfw3.h>
 
 #include <string.h>
 #include <stdio.h>
 
-static const int WIN_W = 16, WIN_H = 16;
+static const int WIN_W = 16, WIN_H = 16, SCL = 2;
 static const double DOUBLE_CLICK_DURATION = 0.2;
 
 static int dragged = 0, paused = 0;
 static double grabX, grabY, lastClick = 0, timer = 0;
+static GLuint prg;
+static GLint movLoc;
 
 static void setIcon(GLFWwindow *win) {
     GLFWimage image = {16, 16, (unsigned char[]) {
@@ -81,20 +84,71 @@ static void cursorPosCallback(GLFWwindow *win, double x, double y) {
     glfwSetWindowPos(win, wx + x - grabX, wy + y - grabY);
 }
 
+static void initGL(void) {
+    const char *VERT =
+    "#version 100\n"
+    "attribute vec2 pos;\n"
+    "uniform vec2 mov, size;\n"
+    "void main() {\n"
+    "    vec2 p = (pos + mov) / size * vec2(2, 2) - vec2(1, 1);\n"
+    "    gl_Position = vec4(p, 0, 1);\n"
+    "}\n";
+    const char *FRAG =
+    "#version 100\n"
+    "precision mediump float;\n"
+    "void main() {\n"
+    "    gl_FragColor = vec4(0, 1, 0, 1);\n"
+    "}\n";
+    prg = glCreateProgram();
+    GLuint vert = glCreateShader(GL_VERTEX_SHADER);
+    GLuint frag = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(vert, 1, &VERT, NULL);
+    glShaderSource(frag, 1, &FRAG, NULL);
+    glCompileShader(vert);
+    glCompileShader(frag);
+    glAttachShader(prg, vert);
+    glAttachShader(prg, frag);
+    glLinkProgram(prg);
+    glDetachShader(prg, frag);
+    glDetachShader(prg, vert);
+    glDeleteShader(frag);
+    glDeleteShader(vert);
+    glUseProgram(prg);
+    movLoc = glGetAttribLocation(prg, "mov");
+    glUniform2f(glGetAttribLocation(prg, "size"), WIN_W, WIN_H);
+    glViewport(0, 0, WIN_W * SCL, WIN_H * SCL);
+    glClearColor(0, 0, 1, 1);
+}
+
+static void exitGL(void) {
+    glDeleteProgram(prg);
+}
+
+static void draw(void) {
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
 int main(void) {
     glfwInit();
+    glfwWindowHint(GLFW_OPENGL_API, GLFW_OPENGL_ES_API);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_DECORATED, 0);
     glfwWindowHint(GLFW_RESIZABLE, 0);
     GLFWwindow *win = glfwCreateWindow(WIN_W, WIN_H, "Stopwatch", NULL, NULL);
+    glfwMakeContextCurrent(win);
     setIcon(win);
     glfwGetCursorPos(win, &grabX, &grabY);
     glfwSetMouseButtonCallback(win, mouseButtonCallback);
     glfwSetCursorPosCallback(win, cursorPosCallback);
+    initGL();
     while (!glfwWindowShouldClose(win)) {
         glfwWaitEventsTimeout(1);
         setTitle(win);
+        draw();
         glfwSwapBuffers(win);
     }
+    exitGL();
     glfwDestroyWindow(win);
     glfwTerminate();
 }
