5308e92147e627848b597895a92781b5b9d9eaf9
Mark-Weston <markweston@cock.li>
2017-10-20 16:09:37 +0300

    Move

--- main.c
+++ main.c
@@ -2,12 +2,25 @@
 
 static const int WIN_W = 16, WIN_H = 16;
 
+static double grabX, grabY;
+
 static void mouseButtonCallback(GLFWwindow *win, int button, int action, int mods) {
-    if (button == GLFW_MOUSE_BUTTON_RIGHT) {
+    if (button == GLFW_MOUSE_BUTTON_LEFT) {
+        if (action == GLFW_PRESS) {
+            glfwGetCursorPos(win, &grabX, &grabY);
+        }
+    } else if (button == GLFW_MOUSE_BUTTON_RIGHT) {
         if (action == GLFW_RELEASE) {
             glfwSetWindowShouldClose(win, 1);
         }
     }
+}
+
+static void cursorPosCallback(GLFWwindow *win, double x, double y) {
+    if (!glfwGetMouseButton(win, GLFW_MOUSE_BUTTON_LEFT)) return;
+    int wx, wy;
+    glfwGetWindowPos(win, &wx, &wy);
+    glfwSetWindowPos(win, wx + x - grabX, wy + y - grabY);
 }
 
 int main(void) {
@@ -15,7 +28,9 @@ int main(void) {
     glfwWindowHint(GLFW_DECORATED, 0);
     glfwWindowHint(GLFW_RESIZABLE, 0);
     GLFWwindow *win = glfwCreateWindow(WIN_W, WIN_H, "Stopwatch", NULL, NULL);
+    glfwGetCursorPos(win, &grabX, &grabY);
     glfwSetMouseButtonCallback(win, mouseButtonCallback);
+    glfwSetCursorPosCallback(win, cursorPosCallback);
     while (!glfwWindowShouldClose(win)) {
         glfwWaitEventsTimeout(1);
         glfwSwapBuffers(win);
