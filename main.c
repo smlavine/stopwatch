29e57665917f666dc6515d5882fb8e1a3e9d47b3
Mark-Weston <markweston@cock.li>
2017-10-20 18:53:12 +0300

    Pause/Resume

--- main.c
+++ main.c
@@ -2,12 +2,23 @@
 
 static const int WIN_W = 16, WIN_H = 16;
 
+static int dragged = 0, paused = 0;
 static double grabX, grabY;
 
 static void mouseButtonCallback(GLFWwindow *win, int button, int action, int mods) {
     if (button == GLFW_MOUSE_BUTTON_LEFT) {
         if (action == GLFW_PRESS) {
             glfwGetCursorPos(win, &grabX, &grabY);
+            dragged = 0;
+        } else if (action == GLFW_RELEASE) {
+            if (!dragged) {
+                paused = !paused;
+                if (paused) {
+                    glfwSetWindowTitle(win, "[Paused]");
+                } else {
+                    glfwSetWindowTitle(win, "Stopwatch");
+                }
+            }
         }
     } else if (button == GLFW_MOUSE_BUTTON_RIGHT) {
         if (action == GLFW_RELEASE) {
@@ -18,6 +29,7 @@ static void mouseButtonCallback(GLFWwindow *win, int button, int action, int mod
 
 static void cursorPosCallback(GLFWwindow *win, double x, double y) {
     if (!glfwGetMouseButton(win, GLFW_MOUSE_BUTTON_LEFT)) return;
+    dragged = 1;
     int wx, wy;
     glfwGetWindowPos(win, &wx, &wy);
     glfwSetWindowPos(win, wx + x - grabX, wy + y - grabY);
