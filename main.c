6df504421eb5245a468a4ea06e96333daea645b2
Mark-Weston <markweston@cock.li>
2017-10-20 13:06:26 +0300

    Open window

--- main.c
+++ main.c
@@ -1,2 +1,16 @@
+#include <GLFW/glfw3.h>
+
+static const int WIN_W = 16, WIN_H = 16;
+
 int main(void) {
+    glfwInit();
+    glfwWindowHint(GLFW_DECORATED, 0);
+    glfwWindowHint(GLFW_RESIZABLE, 0);
+    GLFWwindow *win = glfwCreateWindow(WIN_W, WIN_H, "Stopwatch", NULL, NULL);
+    while (!glfwWindowShouldClose(win)) {
+        glfwWaitEventsTimeout(1);
+        glfwSwapBuffers(win);
+    }
+    glfwDestroyWindow(win);
+    glfwTerminate();
 }
